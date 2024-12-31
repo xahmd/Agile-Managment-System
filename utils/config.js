952c72905c42a7f9f5a1e714f7fd11eaecf73148
Ahmed. <79005095+xahmd@users.noreply.github.com>
2025-01-01 01:41:58 +0800

    سيبسيب

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
-export const serverUrl = "https://fyp-coral.vercel.app/api";
+export const serverUrl = "https://illustrious-gaufre-4af3d9.netlify.app/api";
 // export const serverUrl = "http://localhost:3000/api";
