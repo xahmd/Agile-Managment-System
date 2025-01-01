d9e0c1cbb383e5075b734de0ae0904d75c780d87
Ahmed. <79005095+xahmd@users.noreply.github.com>
2025-01-02 00:20:00 +0800

    config v2

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
-export const serverUrl = "https://illustrious-gaufre-4af3d9.netlify.app/api";
+export const serverUrl = "https://fyp-y3y6.onrender.com/api";
 // export const serverUrl = "http://localhost:3000/api";
