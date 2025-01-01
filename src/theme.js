91dfc54f73bb597c42c1b98da3cd7a5b0b827ce1
Ahmed. <79005095+xahmd@users.noreply.github.com>
2025-01-01 14:16:12 +0800

    asd

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 import {createMuiTheme} from "@material-ui/core/styles";
 import { red } from '@material-ui/core/colors';
 
-// Create a theme instance.
+// Create a theme instance.ss
 const theme = createMuiTheme({
     palette: {
         primary: {
